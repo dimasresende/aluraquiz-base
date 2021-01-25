162385178f1970f12d767c3a05cbcde0c2515a11
Dimas Resende <dresende@uolinc.com>
2021-01-25 20:19:29 -0300

    Criacao do componente QuizContainer

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,54 @@ const BackgroundImage = styled.div`
   background-image: url(${db.bg});
   flex: 1;
   background-size: cover;
-  background-p osition: center;
+  background-position: center;
 `
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid #4caf50;
+  background-color: #1c1814;
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`;
 
 export default function Home() {
   return (
     <BackgroundImage>
-      bla
+      <QuizContainer>
+        <Widget>
+          <h1>The legend of zelda</h1>
+          <p> descrição do quiz</p>
+        </Widget>
+        <Widget>
+
+        </Widget>
+      </QuizContainer>
     </BackgroundImage>
   )
 }
