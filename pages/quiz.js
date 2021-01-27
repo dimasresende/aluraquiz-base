b5a883af02a83130097315fb30e617fcf8379507
Dimas Resende <dresende@uolinc.com>
2021-01-26 21:56:25 -0300

    Estrutura base da pagina de quiz

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 
-export default function QuizPage() {
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizContainer from '../src/components/QuizContainer';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+
+export default function Home() {
+
   return (
-    <div>
-      Página de quiz
-    </div>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz de boardgames</h1>
+          </Widget.Header>
+          <Widget.Content>
+            Questões do Quiz...
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <p>Quiz de terceiros</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/dimasresende" />
+    </QuizBackground>
   );
 }
